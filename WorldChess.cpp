18910722864a4340a7b6853923eab33c35dbf83b
Saransh Arora <74488455+SaranshArora@users.noreply.github.com>
2022-02-13 13:42:57 +0530

    Create WorldChess.cpp

---
+++ WorldChess.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--) {
+	    int x, Cwon=0 , Nwon=0 , Draw=0;
+	    string str;
+	    cin >> x >> str;
+	    for (int i = 0; i< str.length(); i++){
+	        if (str[i] == 'C')
+	            Cwon++;
+	        else if (str[i] == 'N')
+	            Nwon++;
+	       else
+	        Draw++;
+	        
+	    }
+	    if (Cwon > Nwon){
+	        cout << 60*x << endl;
+	    }
+	    if (Cwon < Nwon) {
+	        cout << 40*x << endl;
+	    }
+	    else
+	        cout << 59*x << endl;
+	    
+	}
+	return 0;
+}
